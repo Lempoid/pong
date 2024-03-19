a223ddb71d8c9641ae556a2d2a1faf56159bbfba
Alex <alexbreger123@gmail.com>
2024-03-19 16:21:55 +0200

    finished

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +12,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = ScoreBoard()
 
 
 screen.listen()
@@ -20,7 +25,28 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    # Detect collision with borders
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    # Detect collision with r_puddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    # Detect if R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    # Detect if L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+# Detect if R paddle misses
+    if ball.xcor() < 380:
+        ball.reset_position()
+
 
 screen.exitonclick()
-screen.mainloop()
